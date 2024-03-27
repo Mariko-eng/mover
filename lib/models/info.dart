6fc38b973468e154dda79d294287375d72cef248
Mariko-eng <yigamarkgabriel333@gmail.com>
2024-03-27 17:30:30 +0300

    Adding Bus Company Id & Name To InfoModel

--- lib/models/info.dart
+++ lib/models/info.dart
@@ -7,6 +7,9 @@ class InfoModel {
   final String subTitle;
   final String imageUrl;
   final String description;
+  final String busCompanyId;
+  final String busCompanyName;
+
 
   InfoModel({
     required this.id,
@@ -14,6 +17,8 @@ class InfoModel {
     required this.subTitle,
     required this.imageUrl,
     required this.description,
+    required this.busCompanyId,
+    required this.busCompanyName,
   });
 
   factory InfoModel.fromSnapshot(DocumentSnapshot snap) {
@@ -24,6 +29,8 @@ class InfoModel {
       subTitle: data["subTitle"] ?? "",
       imageUrl: data["imageUrl"] ?? "",
       description: data["description"] ?? "",
+      busCompanyId: data["busCompanyId"] ?? "",
+      busCompanyName: data["busCompanyName"] ?? "",
     );
   }
 }
